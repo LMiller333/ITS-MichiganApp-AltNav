86a9aa02ed3e42a7e490f943eb7ca738a045f042
Miller <linmmill@m-c1mvjr7tj1wk.local>
2018-07-09 17:17:36 -0400

    Adds morning page

---
+++ src/pages/morning/morning.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-morning',
+  templateUrl: 'morning.html'
+})
+export class MorningPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
